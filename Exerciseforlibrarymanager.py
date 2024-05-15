ad55bfa712373398153e75a782a179bca54f9f4e
Pema Wangmo <02230070.cst@rub.edu.bt>
2024-05-15 14:16:37 +0530

    Libray manager

---
+++ Exerciseforlibrarymanager.py
@@ -0,0 +1,8 @@
+name=input("Enter student's name: ")
+age=input("Enter student's age: ")
+grade=input("Enter student's grade: ")
+print("Student inforation added successfully!")
+dict_name={}
+dict_name([(name,{'age':age,'grade':'grade'})])
+search=input("Enter the name of the student to remove or simple enter to skip: t")
+
